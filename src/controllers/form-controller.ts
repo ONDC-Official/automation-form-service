e34bd08ac4fa5479468c54196ff21ed423396201
ParneetkaurDev <parneet.kaur@ondc.org>
2025-12-31 15:46:45 +0530

    Update form-controller.ts for update session data for both transaction id and session id

--- src/controllers/form-controller.ts
+++ src/controllers/form-controller.ts
@@ -84,7 +84,7 @@ export const submitForm = async (req: Request, res: Response) => {
     console.log('Updating session with form data:', formData);
     const submission_id = randomUUID();
     formData.form_submission_id = submission_id;
-
+    await updateSession(formConfig.url, formData, submissionData.transaction_id);
     await updateSession(formConfig.url, formData, submissionData.session_id);
     console.log('Session updated successfully');
     
